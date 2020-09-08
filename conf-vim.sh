1172c9c0a3668f53c4817cef2054930d443e01db
Antonio Faraone <antoniofaraone93@gmail.com>
2020-09-08 12:23:41 +0200

    fix

--- conf-vim.sh
+++ conf-vim.sh
@@ -2,6 +2,12 @@
 
 echo "I'm installing vim-plug ..."
 
+
+if [ ! -x /usr/bin/curl ]; then
+	echo "Please install curl first"
+	exit 1
+fi
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
@@ -9,42 +15,46 @@ echo "I'm adding some useful plugin into your .vimrc..."
 
 touch ~/.vimrc
 
-echo '" Specify a directory for plugins
-" - For Neovim: stdpath('data') . '/plugged'
-" - Avoid using standard Vim directory names like 'plugin'
+mkdir -p /.vim/plugged
+
+
+echo "
 call plug#begin('~/.vim/plugged')
 
-" Make sure you use single quotes
-
-" Shorthand notation; fetches https://github.com/junegunn/vim-easy-align
 Plug 'junegunn/vim-easy-align'
 
-" Any valid git URL is allowed
 Plug 'https://github.com/junegunn/vim-github-dashboard.git'
 
-" Multiple Plug commands can be written in a single line using | separators
 Plug 'SirVer/ultisnips' | Plug 'honza/vim-snippets'
 
-" On-demand loading
 Plug 'scrooloose/nerdtree', { 'on':  'NERDTreeToggle' }
+
 Plug 'tpope/vim-fireplace', { 'for': 'clojure' }
 
-" Using a non-default branch
 Plug 'rdnetto/YCM-Generator', { 'branch': 'stable' }
 
-" Using a tagged release; wildcard allowed (requires git 1.9.2 or above)
 Plug 'fatih/vim-go', { 'tag': '*' }
 
-" Plugin options
 Plug 'nsf/gocode', { 'tag': 'v.20150303', 'rtp': 'vim' }
 
-" Plugin outside ~/.vim/plugged with post-update hook
 Plug 'junegunn/fzf', { 'dir': '~/.fzf', 'do': './install --all' }
 
-" Unmanaged plugin (manually installed and updated)
 Plug '~/my-prototype-plugin'
 
-" Initialize plugin system
-call plug#end()' >> ~/.vimrc
+call plug#end() " >> ~/.vimrc
 
+echo "Theme installing..."
+
+DIR_COLOR=~/.vim/colors/
+mkdir -p $DIR_COLOR
+
+wget "https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim" -P $DIR_COLOR
+
+echo "
+syntax enable
+colorscheme monokai
+set nu
+"  >> ~/.vimrc
+echo "====================================================="
 echo "Now reload .vimrc and then, from vim :PlugInstall"
+echo "====================================================="
