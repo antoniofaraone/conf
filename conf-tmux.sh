fe28b3dd3a7cfb68c3a3628bfb6489e395f3a2be
Antonio Faraone <antoniofaraone93@gmail.com>
2020-09-08 10:51:34 +0200

    Added script to config tmux.

---
+++ conf-tmux.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/arcticicestudio/nord-tmux.git ~/.tmux/themes/nord-tmux
+
+touch ~/.tmux.conf
+
+echo "~/.tmux/themes/nord-tmux/nord-tmux/nord.tmux" >> ~/.tmux.conf
+
+tmux source-file ~/.tmux.conf
+
