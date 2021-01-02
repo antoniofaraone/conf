e7ee40d53aec2b495c13b695e0f2b59c7f73aa23
antonio <antonio@pop-os.localdomain>
2021-01-02 14:48:34 +0100

    Added zsh setup

---
+++ conf-zsh.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+sudo apt install zsh
+
+if [ ! -x /usr/bin/curl ]; then
+	sudo apt install curl
+fi
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
